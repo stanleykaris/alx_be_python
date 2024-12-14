dc7b56c7287d73002d28a06c973c9ad261c28aba
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-15 02:57:51 +0300

    Created a python script that calculates the simple interest earned on an investment over time

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Initializing variables for the formula
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculating the simple interest
+simple_interest = principal * rate * time
+
+# Printing the result of the simple interest calculation
+print("The simple interest is: ", simple_interest)

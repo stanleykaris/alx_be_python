a5c02ec731066c5cb833a53ca99e0c43d4a393ef
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-15 02:08:05 +0300

    Creating a python script file that does basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# Initializing two varibles with integer values
+number1 = 10
+number2 = 5
+
+# Calculating the sum of the two numbers
+sum = number1 + number2
+
+# Calculating the difference between the two numbers
+difference = number1 - number2
+
+# Calculating the product of the two numbers
+product = number1 * number2
+
+# Printing the results of each operation
+print("Addition of {} and {} is {}".format(number1, number2, sum))
+print("Subtraction between {} and {} is {}".format(number1, number2, difference))
+print("Multiplication of {} and {} is {}".format(number1, number2, product))

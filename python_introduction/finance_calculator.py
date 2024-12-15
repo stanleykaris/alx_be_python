608a3a140ff246e4d8ae91e9484b67ccca3daf84
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-15 07:48:52 +0300

    A python script that calculates the user's monthly savings based on inputted monthly income and expenses.

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,12 @@
+monthly_income = int(input("Enter your monthly income: "))
+total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+
+# Calculating the monthly savings
+monthly_savings = monthly_income - total_monthly_expenses
+
+# Calculating the projected savings after one year, incorporating the interest
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+# Printing the user's monthly savings and projected annual savings after including interest
+print("Your monthly savings are ${}".format(monthly_savings))
+print("Projected savings after one year, with interest, is: ", projected_savings)

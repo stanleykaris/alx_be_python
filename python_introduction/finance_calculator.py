8a8d4d5758e500d78a08db7a313a66fda150cf41
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-15 09:40:18 +0300

    Included f-strings in the print statements

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,11 +2,11 @@ monthly_income = int(input("Enter your monthly income: "))
 total_monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 # Calculating the monthly savings
-monthly_savings = int(monthly_income - total_monthly_expenses)
+monthly_savings = monthly_income - total_monthly_expenses
 
 # Calculating the projected savings after one year, incorporating the interest
 projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
 # Printing the user's monthly savings and projected annual savings after including interest
-print("Your monthly savings are $", monthly_savings)
-print("Projected savings after one year, with interest, is: $", projected_savings)
+print(f"Your monthly savings are ${monthly_savings}")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}")

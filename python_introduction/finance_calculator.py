b0af61fe6fd1b164ff9e3fbf9dfd10aa28eb0011
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-15 09:44:47 +0300

    Removed one print statement

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -8,5 +8,4 @@ monthly_savings = monthly_income - total_monthly_expenses
 projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
 # Printing the user's monthly savings and projected annual savings after including interest
-print(f"Your monthly savings are ${monthly_savings}")
 print(f"Projected savings after one year, with interest, is: ${projected_savings}")

12af6827fce07869997309983dc5e8a94783789a
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-15 07:24:15 +0300

    Fixed some changes to the way of calculation

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -2,7 +2,7 @@
 hours = 2
 
 # Calculating the number of seconds
-seconds = hours * 60 * 60
+seconds = hours * 3600
 
 # Printing the result
 print("The number of seconds in {} hours is {}".format(hours, seconds))

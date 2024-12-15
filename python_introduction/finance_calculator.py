6bd33b59f5366b72c1d2ec0bbac640cfe42035ee
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-15 09:55:08 +0300

    Changed data type to float

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,11 @@
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 # Calculating the monthly savings
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_savings = monthly_income - monthly_expenses
 
 # Calculating the projected savings after one year, incorporating the interest
-projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 # Printing the user's monthly savings and projected annual savings after including interest
-print(f"Projected savings after one year, with interest, is: ${projected_savings}")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}")

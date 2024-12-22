f94a8cc7edd4b738eb6d4a042db21e79a3e73808
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-22 21:11:13 +0300

    Changed variable name

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,9 +1,9 @@
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
-operator = input("Choose the operation (+, -, *, /):")
+operation = input("Choose the operation (+, -, *, /):")
 
 # performing calculations using match case
-match operator:
+match operation:
     case "+":
         result = num1 + num2
     case "-":

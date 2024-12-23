62ce52f4b57ecfbf155e6d1912e8918d55dd7578
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-23 13:21:37 +0300

    Removed some of the placeholders in the file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,7 +5,7 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            reminder = f"Reminder:'{task}' is a high priority task that requires immediate attention today!"
+            reminder = f"'{task}' is a high priority task that requires immediate attention today!"
         else:
             reminder = f"'{task}' is a high priority task. Consider completing it when you have the free time."
     
@@ -18,7 +18,7 @@ match priority:
         if time_bound == "yes":
             reminder = f"'{task}' is a low priority task that requires attention today."
         else:
-            reminder = f"Note:'{task}' is a low priority task. Consider completing it when you have the free time."
+            reminder = f"'{task}' is a low priority task. Consider completing it when you have the free time."
     case _:
         reminder = "Invalid priority level entered. Please try again."
     

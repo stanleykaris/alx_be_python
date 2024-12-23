88489c0a290db16f0c3878e22e3f6cdb4445048e
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-23 13:56:30 +0300

    Changed the string variables into print statements

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,21 +5,21 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            reminder = f"'{task}' is a high priority task that requires immediate attention today!"
+            print(f"'{task}' is a high priority task that requires immediate attention today!")
         else:
-            reminder = f"'{task}' is a high priority task. Consider completing it when you have the free time."
+            print(f"'{task}' is a high priority task. Consider completing it when you have the free time.")
     
     case "medium":
         if time_bound == "yes":
-           reminder = f"'{task}' is a medium priority task that requires attention today."
+           print(f"'{task}' is a medium priority task that requires attention today.")
         else:
-            reminder = f"'{task}' is a medium priority task. Consider completing it when you have the free time."
+            print(f"'{task}' is a medium priority task. Consider completing it when you have the free time.")
     case "low":
         if time_bound == "yes":
-            reminder = f"'{task}' is a low priority task that requires attention today."
+            print(f"'{task}' is a low priority task that requires attention today.")
         else:
-            reminder = f"'{task}' is a low priority task. Consider completing it when you have the free time."
+            print(f"'{task}' is a low priority task. Consider completing it when you have the free time.")
     case _:
-        reminder = "Invalid priority level entered. Please try again."
+        print("Invalid priority level entered. Please try again.")
     
 print()

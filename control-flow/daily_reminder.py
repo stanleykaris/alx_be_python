96ec1612956f6fc1a460ac403c7ae70f64616690
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-23 14:05:26 +0300

    Changed the match case statement

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,25 +1,20 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
-time_bound = input("Is it time-bound? (yes/no): ")
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
     case "high":
-        if time_bound == "yes":
-            print(f"'{task}' is a high priority task that requires immediate attention today!")
-        else:
-            print(f"'{task}' is a high priority task. Consider completing it when you have the free time.")
-    
+        reminder = f"'{task}' is a high priority task."
     case "medium":
-        if time_bound == "yes":
-           print(f"'{task}' is a medium priority task that requires attention today.")
-        else:
-            print(f"'{task}' is a medium priority task. Consider completing it when you have the free time.")
+        reminder = f"'{task}' is a medium priority task."
     case "low":
-        if time_bound == "yes":
-            print(f"'{task}' is a low priority task that requires attention today.")
-        else:
-            print(f"'{task}' is a low priority task. Consider completing it when you have the free time.")
+        reminder = f"'{task}' is a low priority task."
     case _:
-        print("Invalid priority level entered. Please try again.")
+        reminder = "Invalid priority level entered. Please try again."
+        
+if time_bound == "yes":
+    print(f"Reminder: {reminder} that requires immediate attention today!")
+else:
+    print(f"Note: {reminder}. Consider completing it when you have free time.")
     
 print()

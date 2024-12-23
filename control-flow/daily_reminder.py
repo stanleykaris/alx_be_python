ab77645bd12102a323190b4d52f68f85bca9e3f9
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-12-23 13:14:24 +0300

    Changes to the output strings of the scrpit file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,9 +5,9 @@ time_bound = input("Is the task time-bound? (yes, no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            reminder = f"Reminder:'{task}' is a high priority task that requires immediate attention today!"
+            reminder = f"'{task}' is a high priority task that requires immediate attention today!"
         else:
-            reminder = f"Reminder:'{task}' is a high priority task. Consider completing it when you have the free time."
+            reminder = f"'{task}' is a high priority task. Consider completing it when you have the free time."
     
     case "medium":
         if time_bound == "yes":
@@ -16,9 +16,9 @@ match priority:
             reminder = f"'{task}' is a medium priority task. Consider completing it when you have the free time."
     case "low":
         if time_bound == "yes":
-            reminder = f"Note:'{task}' is a low priority task that requires attention today."
+            reminder = f"'{task}' is a low priority task that requires attention today."
         else:
-            reminder = f"Note:'{task}' is a low priority task. Consider completing it when you have the free time."
+            reminder = f"'{task}' is a low priority task. Consider completing it when you have the free time."
     case _:
         reminder = "Invalid priority level entered. Please try again."
     

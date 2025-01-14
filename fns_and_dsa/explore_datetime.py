641de81f8400944ef308918cec1d1db9a15646a4
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-01-14 08:38:04 +0300

    A python script that performs specified operations with dates and times

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,19 @@
+import datetime
+from datetime import timedelta
+
+def display_current_datetime():
+    current_datetime = datetime.datetime.now()
+    print(f"Current Date and Time: {current_datetime.strftime("%Y-%m-%d")}")
+    return current_datetime
+
+def calculate_future_date():
+    number_of_days = int(input("Enter the number of days to add: "))
+    current_date = datetime.date.today()
+    future_date = current_date + timedelta(days=number_of_days)
+    print(f"Future date after {number_of_days} days: {future_date.strftime('%Y-%m-%d')}")
+    return future_date
+
+if __name__ == "__main__":
+    display_current_datetime()
+    calculate_future_date()
+    

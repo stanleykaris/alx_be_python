2a360ce9a6e2647a8abc652c2a5ea3f8adc11e92
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-01-19 23:21:49 +0300

    Removed conditional check from main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -10,8 +10,7 @@ def main():
     denominator = sys.argv[2]
 
     result = safe_divide(numerator, denominator)
-    if result is not None:
-        print(f"The result of the division is {result}")
+    print(result)
 
 if __name__ == "__main__":
     main()
